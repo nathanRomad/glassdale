cb2b8fae09a99a59745610800e5c60bd5f90eba5
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-25 13:50:32 -0600

    start of ch3

---
+++ scripts/convictions/ConvictionList.js
@@ -0,0 +1,22 @@
+import { getConvictions, useConvictions } from "./ConvinctionProvider.js"
+import { Conviction } from "./Conviction.js" //need to create
+
+export const ConvictionList = () => {
+    getConvictions()
+      .then(() => {
+        const convictionArray = useConvictions()
+
+        const filters__crime = document.querySelector(".filters")
+        let convictionsHTMLRepresentations = ""
+  
+        for (const conviction of convictionArray) {
+            convictionsHTMLRepresentations += Conviction(conviction)
+        }
+        
+        filters__crime.innerHTML = `
+          <h3>Criminal Convictions</h3>
+          <section class="convictionsList">
+          ${convictionsHTMLRepresentations}
+          </section>`
+      })
+  }
