0b3479b16a93e1cb2bccdcb4e031b6e2e15def28
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-25 16:00:18 -0600

    rendered select tag to DOM with data populated

--- scripts/convictions/ConvictionList.js
+++ scripts/convictions/ConvictionList.js
@@ -1,22 +1,32 @@
-import { getConvictions, useConvictions } from "./ConvinctionProvider.js"
-import { Conviction } from "./Conviction.js" //need to create
 
-export const ConvictionList = () => {
-    getConvictions()
-      .then(() => {
-        const convictionArray = useConvictions()
 
-        const filters__crime = document.querySelector(".filters")
-        let convictionsHTMLRepresentations = ""
+
+
+
+
+
+
+
+// REFERENCE:
+// import { getConvictions, useConvictions } from "./ConvictionProvider.js"
+// import { Conviction } from "./Conviction.js" //need to create
+
+// export const ConvictionList = () => {
+//     getConvictions()
+//       .then(() => {
+//         const convictionArray = useConvictions()
+
+//         const filters__crime = document.querySelector(".filters")
+//         let convictionsHTMLRepresentations = ""
   
-        for (const conviction of convictionArray) {
-            convictionsHTMLRepresentations += Conviction(conviction)
-        }
+//         for (const conviction of convictionArray) {
+//             convictionsHTMLRepresentations += Conviction(conviction)
+//         }
         
-        filters__crime.innerHTML = `
-          <h3>Criminal Convictions</h3>
-          <section class="convictionsList">
-          ${convictionsHTMLRepresentations}
-          </section>`
-      })
-  }
+//         filters__crime.innerHTML = `
+//           <h3>Criminal Convictions</h3>
+//           <section class="convictionsList">
+//           ${convictionsHTMLRepresentations}
+//           </section>`
+//       })
+//   }
--- scripts/criminals/CriminalProvider.js
+++ scripts/criminals/CriminalProvider.js
@@ -11,15 +11,4 @@ export const getCriminals = () => {
                 criminals = parsedCriminals
             }
         )
-    /*
-    Load database state into application state with a fetch().
-    Make sure the last then() updates the criminals array
-    */
 }
-
-
-// Here's the pattern for a fetch call.
-// 1. Request the data
-// 2. Convert the JSON string response to a JavaScript data structure (object or array)
-// 3. Do something with the data
-//.then takes a function expression when called
