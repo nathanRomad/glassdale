6addce1639652342105a3d86c5f56afebbdfab8f
nathanRomad <nathan.a.hamilton@gmail.com>
2021-01-28 14:55:59 -0600

    ch7. rendering form elements.. working on saveButton

--- scripts/notes/noteForm.js
+++ scripts/notes/noteForm.js
@@ -5,39 +5,36 @@ const eventHub = document.querySelector(".container")
 
 const render = () => {
     contentTarget.innerHTML = `
-            <label for="note-suspect">Suspect:</label>
-            <input type="text" id="note-suspect">
+            <label for="date">Date:</label>
+            <input type="date" id="date">
             <label for="note-author">Author:</label>
             <input type="text" id="note-author">
+            <label for="note-suspect">Suspect:</label>
+            <input type="text" id="note-suspect">
             <label for="note-Text">Note:</label>
-            <input type="text" id="note-text">
-            <label for="notesText">Note:</label>
-            <input type="text" id="note-text">
-            <label for="notesText">Note:</label>
             <input type="text" id="note-text">
             <button id="saveNote">Save Note</button>
     `
 }
 
 export const NoteForm = () => {
-    // rest of the code here
     render()
 }
 
-// Handle browser-generated click event in component
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "saveNote") {
-// debugger
-const suspect = document.querySelector("#note-suspect").value  // one way to do it 
-const author =  document.getElementById("note-author").value // another way to do it
-const text = document.querySelector("#note-text").value
+        const date = document.querySelector("#date")
+        const author =  document.getElementById("note-author").value // another way to do it
+        const suspect = document.querySelector("#note-suspect").value  // one way to do it 
+        const note = document.querySelector("#note-text").value
+        const saveNote = document.querySelector("#saveNote")
         // Make a new object representation of a note
         const newNote = {
-            // Key/value pairs here
-            "suspect": suspect,
+            "date": date,
             "author": author, 
-            "text": text,
-            //need to finish the rest of the object keys.
+            "suspect": suspect,
+            "note": note,
+            "saveNote": saveNote,
         }
 
         // Change API state and application state
