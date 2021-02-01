ab7560ff19c5c99250b457ecb65a38a087ae690f
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-01 14:20:31 -0600

    ch7 complete. ch8 started. modal not working, data is working

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -1,3 +1,5 @@
+import { ShowAssociatesButton } from "../associates/ShowAssociatesButton.js"
+
 export const Criminal = (criminal) => {
     return `<section class="criminal__card" id="${criminal.id}"
                 <div class="criminal__name">${criminal.name}<div>
@@ -5,32 +7,10 @@ export const Criminal = (criminal) => {
                 <div class="criminal__conviction">${criminal.conviction}<div>
                 <div class="criminal__termStart">${new Date(criminal.incarceration.start).toLocaleDateString('en-US')}
                 <div class="criminal__termEnd">${new Date(criminal.incarceration.end).toLocaleDateString('en-US')}
-                <button id="associates--${criminal.id}">Associate Alibis</button>
+                <div>${ShowAssociatesButton(criminal)}</div>
                 </section>`
             }
 
 
 
 //dispatch event.
-
-// eventHub.addEventListener("alibiChosen", alibiChosenEvent => {
-//     if ( alibi is clicked, fetch criminal data from getCriminals, use .find to get specific criminal ID,
-//     use criminalID to filter for "known-associates"
-//     display known-associates to DOM using render function/window.alert? )
-    
-// })
-
-// eventHub.addEventListener("crimeChosen", crimeChosenEvent => {
-//     if (crimeChosenEvent.detail.crimeThatWasChosen !== "0") {
-//       const convictionsArray = useConvictions()
-//         const chosenConvictionObject = convictionsArray.find(convictionObj => {
-//         return convictionObj.id === parseInt(crimeChosenEvent.detail.crimeThatWasChosen)
-//       })
-
-//       const criminalsArray = useCriminals()
-//       const filteredCriminalsArray = criminalsArray.filter(
-//         criminalObj => criminalObj.conviction === chosenConvictionObject.name)
-
-//       renderToDom(filteredCriminalsArray)
-//     }
-//   })
