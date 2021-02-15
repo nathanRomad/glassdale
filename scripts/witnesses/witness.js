3861385b7ea5183d5d8900d36f7ff8ef6a61951f
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-15 16:42:54 -0600

    witness.js modified

--- scripts/witnesses/witness.js
+++ scripts/witnesses/witness.js
@@ -1,7 +1,8 @@
-  export const Witness = (witnessObj) => {
-    return `
-        <p class="Witness">
-            ${witnessObj.name}
-        </p>
-    `
-  }
+export const Witness = (witnessObj) => {
+  return `
+      <p class="Witness">
+          Witness: ${witnessObj.name}<br>
+          Statement: "${witnessObj.statements}"
+      </p>
+  `
+}
