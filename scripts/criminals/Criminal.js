e9685439d4faef0425f2e12dbcd63e5f2bfa992e
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-15 11:54:56 -0600

    ch16 finished

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -3,17 +3,17 @@ import { ShowAssociatesButton } from "../associates/ShowAssociatesButton.js"
 export const Criminal = (criminalObject, facilities) => {
     return `
         <section class="criminal__card">
-            <h4>${criminalObject.name}</h4>
+            <h3 class="criminal__name">${criminalObject.name}</h3>
             <div class="criminal__details">
-                <p>Convictions: ${criminalObject.conviction}</p>
-                <p>Arrested by: ${criminalObject.arrestingOfficer}</p>
-                <p>Incarcerated between:
-                    ${new Date(criminalObject.incarceration.start).toLocaleDateString()} and
-                    ${new Date(criminalObject.incarceration.end).toLocaleDateString()}
+                <p class="criminal__conviction">Convictions: ${criminalObject.conviction}</p>
+                <p class="criminal__arrestingOfficer">Arrested by: ${criminalObject.arrestingOfficer}</p>
+                <p class="criminal__incarceration">Incarcerated from:
+                    ${new Date(criminalObject.incarceration.start).toLocaleDateString()} to
+                    ${new Date(criminalObject.incarceration.end).toLocaleDateString()}.
                 </p>
-                <p>Age: ${criminalObject.age}</p>
+                <div class="criminal__age">Age: ${criminalObject.age}</div>
                 <div>
-                    <h2>Facilities</h2>
+                    <h4>Facilities</h4>
                     <ul>
                         ${facilities.map(f => `<li>${f.facilityName}</li>`).join("")}
                     </ul>
