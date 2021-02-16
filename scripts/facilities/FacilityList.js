ff0731c0c65c56e714dfc4a017190c1789eb8b79
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-16 12:04:12 -0600

    facilityList.js in progress

--- scripts/facilities/FacilityList.js
+++ scripts/facilities/FacilityList.js
@@ -33,6 +33,26 @@ const renderFacilities = (facilitiesCollection) => {
         </section>`
     }
 
+// const renderToDom = (criminalsToRender, allFacilities, allRelationships) => {
+//     // Step 1 - Iterate all criminals
+//     criminalsContainer.innerHTML = criminalsToRender.map(
+//         (criminalObject) => {
+//           // Step 2 - Filter all relationships to get only ones for this criminal
+//           const facilityRelationshipsForThisCriminal = allRelationships.filter(cf => cf.criminalId === criminalObject.id)
+//           // debugger
+//             // cannot read FILTER
+//             // Step 3 - Convert the relationships to facilities with map()
+//             const facilities = facilityRelationshipsForThisCriminal.map(cf => {
+//                 const matchingFacilityObject = allFacilities.find(facility => facility.id === cf.facilityId)
+//                 return matchingFacilityObject
+//             })
+    
+//             // Must pass the matching facilities to the Criminal component
+//             return Criminal(criminalObject, facilities)
+//         }
+//     ).join("")
+//   }
+
 
 // Facility List
 // Now it is time to do the reverse of what you did in the last chapter. You listed facilities per criminal, now you are going to list criminals per facility. 
